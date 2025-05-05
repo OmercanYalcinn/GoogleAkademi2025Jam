1027534a260adf2d2f4b15b5558bbfce6b91a646
Kubra <143322363+kbrCoder@users.noreply.github.com>
2025-05-05 18:15:01 +0300

    Add files via upload

--- Assets/Kubra/Script/SceneManager.cs
+++ Assets/Kubra/Script/SceneManager.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager4 : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public void StartGame()
     {
         SceneManager.LoadScene("TestOmercanScene"); 
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Start"); 
     }
 }
